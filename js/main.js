c03e53b037610d8c3acb12b9b9abfb54ac595fcd
Jc Leon <leonjuan123@yahoo.com>
2021-09-22 11:13:38 -0700

    completed user-can-search-for-an-ingredient

--- js/main.js
+++ js/main.js
@@ -2,10 +2,34 @@ var $logo = document.querySelector('.searchbar');
 var $form = document.querySelector('form');
 var $submit = document.querySelector('.search-button');
 
-$submit.addEventListener('click', function (event) {
+function searchFunction() {
   event.preventDefault();
   $logo.append($form);
-  document.querySelector('.search-input').value = '';
-
   $form.classList.remove('new');
-});
+  var searchValue = document.querySelector('.search-input');
+  request(searchValue.value);
+  searchValue.value = '';
+}
+$submit.addEventListener('click', searchFunction);
+
+var arr = [];
+function request(drinks) {
+  event.preventDefault();
+  var xhr = new XMLHttpRequest();
+  xhr.open(
+    'GET',
+    'https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=' + drinks
+  );
+  xhr.responseType = 'json';
+  xhr.addEventListener('load', function () {
+    console.log(xhr.status);
+    console.log(xhr.response);
+    for (var i = 0; i < xhr.response.drinks.length; i++) {
+      var drinkID = xhr.response.drinks[i].idDrink;
+      arr.push(drinkID);
+    }
+    console.log(arr);
+
+  });
+  xhr.send();
+}
