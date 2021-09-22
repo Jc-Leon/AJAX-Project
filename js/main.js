273e66fb791191ed3ab8e0615efa701c4bcd2e54
Jc Leon <leonjuan123@yahoo.com>
2021-09-21 23:26:09 -0700

    completed user-can-search-for-an-ingredient

---
+++ js/main.js
@@ -0,0 +1,10 @@
+var $logo = document.querySelector('.searchbar');
+var $form = document.querySelector('form');
+var $submit = document.querySelector('.search-button');
+
+$submit.addEventListener('click', function (event) {
+  event.preventDefault();
+  $logo.append($form);
+  document.querySelector('.search-input').value = '';
+  $form.classList.remove('remove');
+});
